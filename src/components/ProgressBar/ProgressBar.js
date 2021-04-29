39cda91681100b26f05812f5cbd4d41f34bd664d
Christophe Porteneuve <christophe@delicious-insights.com>
2021-04-29 15:10:25 +0200

    feat(progressbar): first pass at <ProgressBar/>

--- src/components/ProgressBar/ProgressBar.js
+++ src/components/ProgressBar/ProgressBar.js
@@ -1,12 +1,61 @@
+import { COLORS } from '../../constants'
 /* eslint-disable no-unused-vars */
-import React from 'react';
-import styled from 'styled-components';
+import React from 'react'
+import VisuallyHidden from '../VisuallyHidden'
+import styled from 'styled-components'
 
-import { COLORS } from '../../constants';
-import VisuallyHidden from '../VisuallyHidden';
+const DIMS = {
+  large: {
+    padding: 4,
+    height: 24,
+    radius: 8,
+  },
+  medium: {
+    height: 12,
+    radius: 4,
+  },
+  small: {
+    height: 8,
+    radius: 4,
+  },
+}
 
-const ProgressBar = ({ value, size }) => {
-  return <strong>{value}</strong>;
-};
+const BarContainer = styled.div`
+  box-shadow: inset 0px 2px 4px ${COLORS.transparentGray35};
+  background: rgba(128, 128, 128, 0.15);
+  width: 370px;
+  height: ${(props) => DIMS[props.$size].height}px;
+  padding: ${(props) => DIMS[props.$size].padding}px;
+  border-radius: ${(props) => DIMS[props.$size].radius}px;
+  overflow: hidden;
+`
 
-export default ProgressBar;
+const Bar = styled.div`
+  background-color: #4747eb;
+  width: ${(props) => props.$value}%;
+  height: 100%;
+  border-radius: ${(props) =>
+    props.$size === 'large'
+      ? props.$value >= 99.5
+        ? '4px'
+        : '4px 0 0 4px'
+      : 'none'};
+`
+
+const ProgressBar = ({ value, size = 'medium' }) => {
+  return (
+    <BarContainer
+      role='progressbar'
+      aria-valuenow={value}
+      aria-valuemin={0}
+      aria-valuemax={100}
+      $size={size}
+    >
+      <Bar $size={size} $value={value}>
+        <VisuallyHidden>{value}%</VisuallyHidden>
+      </Bar>
+    </BarContainer>
+  )
+}
+
+export default ProgressBar
